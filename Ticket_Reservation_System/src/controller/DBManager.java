d480f3bafb0af38ed2b187fe42f297285bb5e84e
albinrad <50343920+albinrad@users.noreply.github.com>
2020-11-25 02:40:30 -0700

    Modified DBManager with some functionality

--- Ticket_Reservation_System/src/controller/DBManager.java
+++ Ticket_Reservation_System/src/controller/DBManager.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import model.*;
@@ -9,15 +14,30 @@ import model.*;
 // Suggestion - Fill in all the data instead of just having getter functions?
 //				e.g. get all movies from table in DB and fill in movieList in Theater
 public class DBManager {
+	private Connection dbConnection;
 	
-	public void connectToDB(String url, String u, String p) {
-		
+	public void connectToDB(String url, String u, String p) throws SQLException {
+		dbConnection = DriverManager.getConnection(url, u, p);
 	}
 	
-	public void addUser() {
-		
+	public void addUser(String email, String pWord, String cardNum, String bankName) throws SQLException {
+		Statement dbStatement = dbConnection.createStatement();
+		dbStatement.executeUpdate("INSERT INTO 'users' "
+				+ "VALUES ('" + email + "','" + pWord + "','" + cardNum + "','" + bankName + "')");
 	}
-
+	
+	public ArrayList<Movie> getAllMovies() throws SQLException {
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		Statement dbStatement = dbConnection.createStatement();
+		ResultSet moviesResult = dbStatement.executeQuery("SELECT * FROM db.movielist");
+		while(moviesResult.next()) {
+			String movieName = moviesResult.getString("name");
+			// load in remainder of movie's data
+		}
+		return movies;
+	}
+	
+	// May continue this way - will discuss further during wed's lecture -Albin
 	public ArrayList<Showtime> getShowtimes() {
 		return null;
 	}
@@ -26,8 +46,12 @@ public class DBManager {
 		return null;
 	}
 	
-	public Account getAccount(String email) {
-		return null;
+	public Account getAccount(String email) throws SQLException {
+		Account newAccount = new Account(email, null, email, null); // TO BE FIXED
+		Statement dbStatement = dbConnection.createStatement();
+		ResultSet accountResult = dbStatement.executeQuery("SELECT * FROM db.accountlist WHERE account_email = '" + email + "'");
+		
+		return newAccount;
 	}
 	
 	
