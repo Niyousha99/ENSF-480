0814852c5cb3bbb767b29489cbfec85bf2585606
albinrad <50343920+albinrad@users.noreply.github.com>
2020-11-27 13:38:20 -0700

    Added DB credentials to constants class

--- Ticket_Reservation_System/src/model/Constants.java
+++ Ticket_Reservation_System/src/model/Constants.java
@@ -5,4 +5,8 @@ public interface Constants {
 	public static final int SEATROWS = 3;
 	public static final int SEATCOLS = 6;
 	
+	// Credentials to connect to database
+	public static final String DBURL = "jdbc:mysql://localhost:8000/ticket_reservation_backend";
+	public static final String DBUSERNAME = "root";
+	public static final String DBPASSWORD = "root";
 }
